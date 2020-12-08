44cd11f3fc5c0f03c0c00005191bfcd069352a1c
Ange Flores Moreno <angelflores992@gmail.com>
2020-12-08 12:07:31 -0600

    Simple GET added

--- index.js
+++ index.js
@@ -10,13 +10,9 @@ app.use(cors())
 
 // Not get for now
 
-// app.get("/", async (req, res) => {
-// 	res.send("This is working")
-// 	const { link } = req.body
-// 	console.log(req.body)
-// 	// const product = await cyberpuertaScrap(link)
-//    // res.json(product)
-// })
+app.get("/", async (req, res) => {
+	res.send("This is working")
+})
 
 app.post("/", async (req, res) => {
 	const { link } = req.body
