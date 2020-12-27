bbb743670ed61b8d482bb71791b113d67f470aec
Ange Flores Moreno <angelflores992@gmail.com>
2020-12-26 22:12:28 -0700

    CORS

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080
 const { cyberPuertaScrap } = require("./scrapper")
 
 app.use(express.json())
-app.use(cors({origin: "http://localhost:3000/"}))
+app.use(cors({origin: "*"}))
 
 app.get("/", (req, res) => {
 	res.send("This is working")
